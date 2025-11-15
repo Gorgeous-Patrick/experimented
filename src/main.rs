0cfb27f728f46ce547389b86da5fbe47f6607e42
Patrick Li <baichuanli@yahoo.com>
2025-11-14 19:34:38 -0500

    feat: greeting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Parser, Subcommand};
+use experimented::{greet};
+
+#[derive(Parser)]
+#[command(name = "experimented")]
+struct Cli{
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Greet {
+        name: String
+    },
+    SomethingElse
+}
+
 fn main() {
     println!("Hello, world!");
+    let cli = Cli::parse();
+    match cli.command {
+        Command::Greet { name } => greet(),
+        Command::SomethingElse => {println!("SOMETHING")}
+    }
 }
